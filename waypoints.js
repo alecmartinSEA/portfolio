4ba3e999c826fd6cd697743b2b6f0bde9cec413c
Alec Martin <alecmartin28@gmail.com>
2017-08-06 18:29:00 -0700

    added the js files and cchanged html/css files

---
+++ waypoints.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+	// hide our element on page load
+  $('.test').css('opacity', 0);
+ 
+  $('.test').waypoint(function() {
+      $('.test').addClass('fadeInLeft');
+  }, { offset: '30%' });
+ 
+
+    
+});
